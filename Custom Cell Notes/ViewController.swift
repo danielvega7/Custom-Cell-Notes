4b0881acb0ef9762cc189e44d2f5552dfc76c6cd
Daniel Vega <daniel.vega23@d155.org>
2021-11-05 07:28:53 -0500

    Initial Commit

---
+++ Custom Cell Notes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Custom Cell Notes
+//
+//  Created by DANIEL VEGA on 11/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
