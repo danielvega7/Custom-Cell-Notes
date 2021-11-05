ebd7643ea23727c8c9f9819ca3f41be410c76492
Daniel Vega <daniel.vega23@d155.org>
2021-11-05 08:09:27 -0500

    custom cell notes

--- Custom Cell Notes/ViewController.swift
+++ Custom Cell Notes/ViewController.swift
@@ -7,11 +7,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+public struct Student{
+    var first: String
+    var last: String
+    var favColor: String
+    var id: Int
+    var lunch: Int
+    var favAnimal: String
+}
 
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+
+    @IBOutlet weak var tableViewOutlet: UITableView!
+    var students = [Student]()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableViewOutlet.delegate = self
+        tableViewOutlet.dataSource = self
+        students.append(Student(first: "Danny", last: "Vega", favColor: "Blue", id: 230001, lunch: 5, favAnimal: "Penguin"))
+        students.append(Student(first: "Karson", last: "Hollander", favColor: "Red", id: 230420, lunch: 7, favAnimal: "Dog"))
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        students.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath) as! MyCustomCell4Life
+        cell.firstLabelOutlet.text = students[indexPath.row].first
+        cell.lastLabelOutlet.text = students[indexPath.row].last
+        cell.imageViewOutlet.backgroundColor = UIColor.blue
+        return cell
     }
 
 
